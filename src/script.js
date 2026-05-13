38e34647ce5de39040a37562401e1df2db459b8f
André Dias <57869108+Andredev-dias@users.noreply.github.com>
2026-05-13 09:40:54 -0300

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -7,3 +7,11 @@ window.addEventListener('scroll', ()=> {
         nav.classList.remove('sc')
     }
 })
+
+
+function navTo(id){
+    document.getElementById(id).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    })
+}
